f8e3454ad813a8e718b9200f37a82f026dae8143
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-03-05 13:37:27 +0530

    chore: prettier cjs

---
+++ prettier.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('prettier').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  bracketSpacing: true,
+  bracketSameLine: true,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  endOfLine: "auto",
+  printWidth: 110,
+  semi: true,
+  tabWidth: 2,
+  trailingComma: "es5",
+};
