9dc13c0ec03064e5bdc5b6f596f9630d1d41cb9e
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-03-06 23:09:38 +0530

    feat: reusable gradient button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/button";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10" href="#login">
+          check
+        </Button>
+      </div>
+      <ButtonGradient />
+    </>
+  );
 }
 
 export default App;
